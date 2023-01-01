62e90dc9496069fb045c033709db01a135975f20
AZZIE2000 <azzam.faraj0@gmail.com>
2023-01-01 18:26:15 +0300

    done

--- src/pages/PlayingTable.jsx
+++ src/pages/PlayingTable.jsx
@@ -4,32 +4,32 @@ import Card from "../components/Card";
 import FlippedCard from "../components/FlippedCard";
 import { GameContext } from "../context/GameContext";
 export default function PlayingTable() {
-	const { playerOneCards, playerTwoCards, ready } = useContext(GameContext);
-	return (
-		<>
-			<div className="h-screen items-center flex flex-col  justify-between ">
-				<div className="flex ">
-					{ready &&
-						playerOneCards?.map((card) => {
-							return <Card card={card} />;
-						})}
-				</div>
-				<div className=" h-fit flex justify-between px-10 w-full ">
-					<FlippedCard />
-					   {
+    const { playerOneCards, playerTwoCards, ready, ground, drawCard } = useContext(GameContext);
+    return (
+        <>
+            <div className="h-screen items-center flex flex-col  justify-between ">
+                <div className="flex ">
+                    {ready &&
+                        playerTwoCards?.map((card) => {
+                            return <Card player={'two'} card={card} />;
+                        })}
+                </div>
+                <div className=" h-fit flex justify-between px-10 w-full ">
+                    <FlippedCard />
+                    {
                         ready &&
-                        <Card card={ground[0]} />
+                        <Card ground={true} card={ground[ground?.length - 1]} />
                     }
-					<div></div>
-				</div>
-				<div className="flex ">
-					{ready &&
-						playerTwoCards?.map((card) => {
-							return <Card card={card} />;
-						})}
-				</div>
-			</div>
-		</>
-	);
+                    <div></div>
+                </div>
+                <div className="flex ">
+                    {ready &&
+                        playerOneCards?.map((card) => {
+                            return <Card player={'one'} card={card} />;
+                        })}
+                </div>
+            </div>
+        </>
+    );
 
 }
