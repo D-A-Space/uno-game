19a5732f3c66440e821168b9fd5c0dbc2d9a5929
DuaaNawwas <duaa.nawwas@gmail.com>
2023-01-01 17:20:33 +0300

    fixed card and added flipped card

--- src/pages/PlayingTable.jsx
+++ src/pages/PlayingTable.jsx
@@ -1,36 +1,30 @@
-import React, { useContext } from 'react'
-import Card from '../components/Card'
-import { GameContext } from '../context/GameContext'
+import React, { useContext } from "react";
+import Card from "../components/Card";
+import FlippedCard from "../components/FlippedCard";
+import { GameContext } from "../context/GameContext";
 export default function PlayingTable() {
-    const { playerOneCards, playerTwoCards, ready } = useContext(GameContext)
-    return (
-        <>
-            <div className='h-screen items-center flex flex-col   '>
-                <div className='flex scale-50'>
-                    {
-                        ready &&
-                        playerOneCards?.map(card => {
-
-                            return <Card card={card} />
-                        })
-                    }
-
-                </div>
-                <div className='scale-50 h-fit '>
-                    <Card card={'b8'} />
-                </div>
-                <div className='flex scale-50'>
-                    {
-                        ready &&
-                        playerTwoCards?.map(card => {
-
-                            return <Card card={card} />
-                        })
-                    }
-
-                </div>
-
-            </div>
-        </>
-    )
+	const { playerOneCards, playerTwoCards, ready } = useContext(GameContext);
+	return (
+		<>
+			<div className="h-screen items-center flex flex-col  justify-between ">
+				<div className="flex ">
+					{ready &&
+						playerOneCards?.map((card) => {
+							return <Card card={card} />;
+						})}
+				</div>
+				<div className=" h-fit flex justify-between px-10 w-full ">
+					<FlippedCard />
+					<Card card={"b8"} />
+					<div></div>
+				</div>
+				<div className="flex ">
+					{ready &&
+						playerTwoCards?.map((card) => {
+							return <Card card={card} />;
+						})}
+				</div>
+			</div>
+		</>
+	);
 }
